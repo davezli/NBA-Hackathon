b7bb6f776397cf13e34efe895af9482053774d20
Dave Li <daveli@Daves-MacBook-Air.local>
2016-09-24 17:28:16 -0400

    Removing unused files

--- Calculation.R
+++
@@ -1,9 +0,0 @@
-personId <- uniquePlayerNames[uniquePlayerNames$Name==input%playerId,]$Person_id
-shots <- rbind(shotSummary15[shotSummary15$PERSON_ID==personId,], shotSummary16[shotSummary16$PERSON_ID==personId,])
-x <- shots[,c(15,19]
-y <- shots[,10]
-model = train(x,y,'nb',trControl=trainControl(method='cv',number=10))
-z <- x[1,1:2]
-z[1] <- input%shotDist
-z[2] <- input%closeDefDist
-predict(model$finalModel,z)
