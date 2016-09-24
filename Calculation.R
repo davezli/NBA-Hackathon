9d3161d900d89ec687539b7e6ca0aefbef2cfd84
MikeZWang <mzwang95@gmail.com>
2016-09-24 15:02:37 -0400

    More calculations

--- Calculation.R
+++ Calculation.R
@@ -1,2 +1,6 @@
 personId <- uniquePlayerNames[uniquePlayerNames$Name=="Austin Rivers",]$Person_id
 shots <- rbind(shotSummary15[shotSummary15$PERSON_ID==personId,], shotSummary16[shotSummary16$PERSON_ID==personId,])
+shotsResultMap <- c("missed"=0, "made"=1)
+mappedResult <- shotsResultMap[as.character(shots$SHOT_RESULT)]
+result <- mean(mappedResult)
+result
