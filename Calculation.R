f5b393e894d7ca47068220c268503fef5d7af704
MikeZWang <mzwang95@gmail.com>
2016-09-24 16:14:08 -0400

    Machine Learning Algo

--- Calculation.R
+++ Calculation.R
@@ -1,6 +1,9 @@
-personId <- uniquePlayerNames[uniquePlayerNames$Name=="Austin Rivers",]$Person_id
+personId <- uniquePlayerNames[uniquePlayerNames$Name==input%playerId,]$Person_id
 shots <- rbind(shotSummary15[shotSummary15$PERSON_ID==personId,], shotSummary16[shotSummary16$PERSON_ID==personId,])
-shotsResultMap <- c("missed"=0, "made"=1)
-mappedResult <- shotsResultMap[as.character(shots$SHOT_RESULT)]
-result <- mean(mappedResult)
-result
+x <- shots[,c(15,19]
+y <- shots[,10]
+model = train(x,y,'nb',trControl=trainControl(method='cv',number=10))
+z <- x[1,1:2]
+z[1] <- input%shotDist
+z[2] <- input%closeDefDist
+predict(model$finalModel,z)
